c0f916fae5d13630308ada2705298eaa22c74a23
Suntrax <qwertzgamer1@gmail.com>
2026-06-21 00:35:09 +0200

    ServerSelectorButton fixed imports and warnings

--- app/src/main/java/com/blissless/tensei/ui/screens/player/ServerSelectorButton.kt
+++ app/src/main/java/com/blissless/tensei/ui/screens/player/ServerSelectorButton.kt
@@ -2,7 +2,6 @@ package com.blissless.tensei.ui.screens.player
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
