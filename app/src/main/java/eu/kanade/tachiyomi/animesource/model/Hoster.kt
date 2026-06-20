d1d62b0365a7504a90b2f6542a187bdeb7e7045b
Suntrax <qwertzgamer1@gmail.com>
2026-06-21 01:47:34 +0200

    Hoster fixed imports and warnings

--- app/src/main/java/eu/kanade/tachiyomi/animesource/model/Hoster.kt
+++ app/src/main/java/eu/kanade/tachiyomi/animesource/model/Hoster.kt
@@ -4,20 +4,7 @@ class Hoster(
     val hosterUrl: String = "",
     val hosterName: String = "",
     val videoList: List<Video>? = null,
-    val internalData: String = "",
     val lazy: Boolean = false,
 ) {
-    companion object {
-        const val NO_HOSTER_LIST = "no_hoster_list"
-
-        fun List<Video>.toHosterList(): List<Hoster> {
-            return listOf(
-                Hoster(
-                    hosterUrl = "",
-                    hosterName = NO_HOSTER_LIST,
-                    videoList = this,
-                ),
-            )
-        }
-    }
+    companion object
 }
