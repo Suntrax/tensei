2ef5c40d681dd76d98b8244624734170abe9a620
Suntrax <qwertzgamer1@gmail.com>
2026-06-21 01:50:22 +0200

    AnimeSourceFactory fixed imports and warnings

--- app/src/main/java/eu/kanade/tachiyomi/animesource/ConfigurableAnimeSource.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.animesource
-
-import androidx.preference.PreferenceScreen
-
-interface ConfigurableAnimeSource {
-    fun setupPreferenceScreen(screen: PreferenceScreen)
-}
