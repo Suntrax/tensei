db5ff82fd414589b8d0f2f11e1bd1be0389d2121
Suntrax <qwertzgamer1@gmail.com>
2026-07-21 18:47:34 +0200

    fixed backbutton not properly closing the stream playback

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 11
-        versionName = "1.1.0git"
+        versionName = "1.1.0"
 
         val anilistApiKey = localProperties.getProperty("CLIENT_ID_ANILIST")
         val tmdbApiKey = localProperties.getProperty("TMDB_API_KEY")
